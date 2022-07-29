d6c2f84c5486ddfb18aa4f0f57686c3cf319ac12
vincent verbergt <vincent.verbergt@gmail.com>
2022-07-30 01:05:57 +0200

    basic folder structure

---
+++ lib/about/about.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class About extends StatelessWidget {
+  const About({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
