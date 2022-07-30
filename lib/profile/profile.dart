a01eb49515534ac263889e6be64f0b77b3eec527
vincent verbergt <vincent.verbergt@gmail.com>
2022-07-30 02:52:20 +0200

    added signout function

--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -1,16 +1,22 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:quiz/shared/bottom_navbar.dart';
-
+import 'package:quiz/services/auth.dart';
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: BottomNavBar(),
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('profile'),
+      ),
+      body: ElevatedButton(
+        onPressed: () async {
+          await AuthService().singOut();
+          // ignore: use_build_context_synchronously
+          Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+        },
+        child: const Text('Signout'),
+      ),
     );
   }
 }
