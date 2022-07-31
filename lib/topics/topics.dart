6af317e173b61fa1792132d037f3319dc2356913
vincent verbergt <vincent.verbergt@gmail.com>
2022-07-31 17:06:47 +0200

    made error screen

--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:quiz/services/firestore.dart';
+import 'package:quiz/services/models.dart';
 import 'package:quiz/shared/bottom_navbar.dart';
+import 'package:quiz/shared/error.dart';
+import 'package:quiz/shared/loading.dart';
 
 class TopicsScreen extends StatelessWidget {
   const TopicsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: BottomNavBar(),
+    return FutureBuilder<List<Topic>>(
+      future: FireStoreService().getTopics(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const LoadingScreen();
+        } else if (snapshot.hasError) {
+          return const ErrorMessage(message: snapshot.hasError.toString());
+        } else if (snapshot.hasData) {
+
+        } else {
+
+        }
+      },
     );
   }
 }
