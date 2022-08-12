6e305fe94d253ea035ac9af33db4691356053220
vincent verbergt <vincent.verbergt@gmail.com>
2022-08-12 22:43:30 +0200

    added Provider and streamprovider

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:quiz/firebase/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:quiz/routes.dart';
+import 'package:quiz/services/firestore.dart';
+import 'package:quiz/services/models.dart';
 import 'package:quiz/theme.dart';
+import 'package:provider/provider.dart';
 // fierbase web id: 1:674191127017:web:b0f2328d3e3446a4579b78
 
 // flutter run -d chrome --web-hostname localhost --web-port 5000
@@ -21,10 +24,14 @@ class FlutterQuiz extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: appTheme,
-      routes: appRoutes,
+    return StreamProvider(
+      create: (_) => FireStoreService().streamReport(),
+      initialData: Report(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: appTheme,
+        routes: appRoutes,
+      ),
     );
   }
 }
